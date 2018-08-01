bfc7066936655e0ad30c1696042266523885e44b
Daniel Kang <danielkang674@gmail.com>
2018-08-01 18:57:35 -0500

    pseudo complete

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,38 @@
-var Queue = function() {
+var Queue = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+let anotherInstance = new Queue();
 
+Queue.prototype.enqueue = function (value) {
+  let tail = this.size();
+  for (let key in this.storage) {
+    if (tail <= Number(key)) {
+      tail = Number(key);
+    }
+  }
+  this.storage[tail + 1] = value;
+};
+
+Queue.prototype.dequeue = function () {
+  let head = this.size();
+  let value;
+  for (let key in this.storage) {
+    if (head >= Number(key)) {
+      head = Number(key);
+    }
+  }
+  value = this.storage[head];
+  delete this.storage[head];
+  return value;
+};
+
+Queue.prototype.size = function () {
+  let counter = 0;
+  for (let key in this.storage) {
+    counter++;
+  }
+  return counter;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,7 +1,26 @@
-var Stack = function() {
+var Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  
+  this.storage = {};
+};
+let someInstance = new Stack();
+
+Stack.prototype.push = function (value) {
+  let tail = this.size();
+  this.storage[tail] = value;
 };
 
+Stack.prototype.pop = function () {
+  let tail = this.size();
+  let value = this.storage[tail - 1];
+  delete this.storage[tail - 1];
+  return value;
+};
 
+Stack.prototype.size = function () {
+  let counter = 0;
+  for (let key in this.storage) {
+    counter++;
+  }
+  return counter;
+};
