4e3803fe6cff3ee4149b1e2a0bb255d8346e1d1b
Daniel Kang <danielkang674@gmail.com>
2018-08-01 19:08:39 -0500

    Sprint 1 complete

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,35 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    let tail = this.size();
+    for (let key in this.storage) {
+      tail = tail <= Number(key) ? Number(key) + 1 : tail;
+    }
+    this.storage[tail] = value;
+  }
+
+  dequeue() {
+    let head = this.size();
+    let value;
+    for (let key in this.storage) {
+      head = head >= Number(key) ? Number(key) : head;
+    }
+    value = this.storage[head];
+    delete this.storage[head];
+    return value;
+  }
+
+  size() {
+    let counter = 0;
+    for (let key in this.storage) {
+      counter++;
+    }
+    return counter;
+  }
 }
+
+let es6Queue = new Queue;
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,28 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    let tail = this.size();
+    this.storage[tail] = value;
+  }
+
+  pop() {
+    let tail = this.size();
+    let value = this.storage[tail - 1];
+    delete this.storage[tail - 1];
+    return value;
+  }
+
+  size() {
+    let counter = 0;
+    for (let key in this.storage) {
+      counter++;
+    }
+    return counter;
+  }
 }
+
+let es6Instance = new Stack();
