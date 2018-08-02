f7716d8ad7267bf500190893df57293b2d8a5fee
Daniel Kang <danielkang674@gmail.com>
2018-08-02 17:17:27 -0500

    Set and extra test, still need graph tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,27 @@
-var Set = function() {
+var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function (item) {
+  this._storage[item] = 1;
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function (item) {
+  if (this._storage[item]) {
+    return true;
+  }
+  return false;
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function (item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * all functions are constant complexity
  */
