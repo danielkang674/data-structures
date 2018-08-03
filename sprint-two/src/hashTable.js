ee64cc468a111ae314c37927292cddaba3b8c30a
Daniel Kang <danielkang674@gmail.com>
2018-08-02 19:15:19 -0500

    Hash table easy complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,38 @@
 
 
-var HashTable = function() {
+var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (!this._storage.get(index)) {
+    this._storage.set(index, []);
+  } 
+  this._storage.get(index).push([k, v]);
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let container = this._storage.get(index);
+  let value;
+  for (let i = 0; i < container.length; i++) {
+    if (container[i][0] === k) {
+      value = container[i][1];
+    }
+  }
+  return value;
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let container = this._storage.get(index);
+  for (let i = 0; i < container.length; i++) {
+    if (container[i][0] === k) {
+      delete container[i][0];
+    }
+  }
 };
 
 
